235d1fecd62d2319044227f71ccd30f09eeb9ef6
= <ksh091411@naver.com>
2021-05-27 14:19:49 +0900

    20210527

---
+++ 1934.py
@@ -0,0 +1,21 @@
+#백준 제출 > 시간초과
+#아마 for 문으로 돌려서 숫자가 둘다 10000자리 이상일 경우 오래걸리는 것으로 예상
+c=int(input())
+
+
+for i in range(c):
+    j=0
+    a,b=map(int,input().split())
+
+    if (a==1 and b==1):
+
+        print(a)
+
+    else:
+
+        for j in range(2,a*b+1):
+
+            if ( j%a==0 and j%b==0 ):
+                print(j)
+                break
+
