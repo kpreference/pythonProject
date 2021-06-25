9685a5f7573411c2a9cb845cf990b22900a1583a
= <ksh091411@naver.com>
2021-06-25 22:02:37 +0900

    20210625

--- 1157.py
+++ 1157.py
@@ -1,32 +1,26 @@
-inp=input()
+n=input()
 
-wrd=[]
-cnt=[]
+bst=0
+bstn=0
+al=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-
-for i in inp:
-
-    if ord(i)>96 and ord(i)<123:
-
-        if wrd.find(chr(ord(i)-32) == -1 :
-            wrd.append(chr(ord(i) - 32))
-            cnt[wrd.find(chr(ord(i) - 32)]+=1
-
-        else:
-
-            cnt[wrd.find(chr(ord(i) - 32)] += 1
-
-
-    elif ord(i)>64 and ord(i)<91:
-        if wrd.find(chr(ord(i)) != -1:
-            wrd.append(chr(ord(i)))
-        cnt[wrd.find(chr(ord(i))] += 1
-
-        else:
-        cnt[wrd.find(chr(ord(i))] += 1
-cnt.sort()
-print(cnt[0])
-
-
-
-#AZaz 65 90 97 122
+for i in n:
+    if ord(i)>64 and ord(i)<91:
+        al[ord(i)-65]+=1
+    elif ord(i)>96 and ord(i)<123:
+        al[ord(i) - 97] += 1
+p=0
+for j in range(0,26):
+    p=0
+    if al[j]!=0 and al[j]==bst:
+        for k in range(j,26):
+            if al[j]<al[k]:
+                p+=1
+        if p==0:
+            print("?")
+            break
+    elif al[j]>bst:
+        bst=al[j]
+        bstn=j
+if j==25:
+    print(chr(bstn+65))
