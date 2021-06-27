73681fbf51c22d634fddbf37106f0e6a6a6bb10d
= <ksh091411@naver.com>
2021-06-27 22:44:34 +0900

    20210627

---
+++ 1934(2).py
@@ -0,0 +1,26 @@
+'''
+'# include<stdio.h>
+int
+main()
+{
+    int
+n = 0;
+int
+a, b, i, j;
+scanf("%d", & n);
+for (i=0;i < n;i++)
+{
+    scanf("%d %d", & a, & b);
+
+for (j=2;j < 2147483647;j++)
+{
+if ( (j % a == 0) & & (j % b == 0) )
+{
+printf("%d\n", j);
+break;
+}
+}
+
+}
+
+}'''
