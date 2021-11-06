387d152fcc3f970d029495808b2a1364898e1406
= <ksh091411@naver.com>
2021-11-06 13:01:44 +0900

    20211106

--- test.py
+++ test.py
@@ -1,24 +1,7 @@
-import math
+n=int(input())
 cnt=0
-while(1):
-    cnt+=1
-    a,b,c=map(int,input().split())
-    if a==b and b==c and a==0:
-        break
-    print("Triangle #%d"%cnt)
-
-    if c==(-1):
-        print("c = %.3f"%(math.sqrt(a*a+b*b)))
-    elif b==(-1):
-        if a==c or a>c:
-            print("Impossible.")
-        else:
-            print("b = %.3f" % (math.sqrt(c*c - a*a)))
-    elif a==(-1):
-        if b==c or b>c:
-            print("Impossible.")
-        else:
-            print("a = %.3f" % (math.sqrt(c*c - b*b)))
-
-
-    print()
+a=[int(x) for x in input().split()]
+for i in range(n):
+    if a[i]!=(i+1):
+        cnt+=1
+print(cnt)
